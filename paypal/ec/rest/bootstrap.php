5c7667d14bfa1752eb3b4960fe4994826f73e61d
Saavedra <jusaavedra@paypalcorp.com>
2017-03-06 16:01:24 +0000

    ssss

--- paypal/ec/rest/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-$apiContext = new ApiContext(
-	new OAuthTokenCredential(
-		'ATLrdYxHR7VT7w_XeIy82ElmfcRLZGxbmVKgbA7GNZ9SDtImNWqm18_FpV6W3sA7RJ4X_wzWw1LKI8pW',
-		'EBPCf4KGvhO1ZB56JGqWymTbKpvN8Pdi5Gcej-Igxa22bVgxLWNyESKCMX7pZjd3AjokRyT3-jxPgac_'
-	)
-);
-?>
