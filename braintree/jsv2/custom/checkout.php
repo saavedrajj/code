22ba79bd8ae809a43e38a5cfda76c89efd5ca66c
Juan Saavedra <Juan Saavedra>
2017-04-02 01:04:05 +0100

    sadsd

--- braintree/jsv2/custom/checkout.php
+++ braintree/jsv2/custom/checkout.php
@@ -7,15 +7,47 @@
 	<title>Checkout</title>
 </head>
 <body>
-	<?php	
+	<?php
+
 	$result = Braintree_Transaction::sale([
 		'amount' => '1.00',
 		'paymentMethodNonce' => $nonceFromTheClient,
+		'customer' => [
+		'firstName' => 'John',
+		'lastName' => 'Doe',
+		'company' => 'Acme Ltd',
+		'phone' => '312-555-1234',
+		'fax' => '312-555-1235',
+		'website' => 'http://www.example.com',
+		'email' => 'john@smith.com'
+		],
+		'billing' => [
+		'firstName' => 'John',
+		'lastName' => 'Doe',
+		'company' => 'Acme Ltd',
+		'streetAddress' => '1 E Main St',
+		'extendedAddress' => 'Suite 403',
+		'locality' => 'Chicago',
+		'region' => 'IL',
+		'postalCode' => '60622',
+		'countryCodeAlpha2' => 'US'
+		],
+		'shipping' => [
+		'firstName' => 'John',
+		'lastName' => 'Doe',
+		'company' => 'Acme Ltd',
+		'streetAddress' => '1 E 1st St',
+		'extendedAddress' => 'Suite 403',
+		'locality' => 'Bartlett',
+		'region' => 'IL',
+		'postalCode' => '60103',
+		'countryCodeAlpha2' => 'US'
+		],
 		'options' => [
 		'submitForSettlement' => True
 		]
 		]);
-	?>
-	<pre><?php print_r($result); ?></pre>	
-</body>
-</html>
+		?>
+		<pre><?php print_r($result); ?></pre>	
+	</body>
+	</html>
--- braintree/jsv2/custom/index.php
+++ braintree/jsv2/custom/index.php
@@ -1,23 +1,30 @@
 <?php require_once("../../includes/braintree_init_ie.php"); ?>
 <?php $clientToken = Braintree_ClientToken::generate(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Drop-in Payment UI</title>
+	<title>Custom Payment</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-<form id="checkout" method="post" action="checkout.php">
-		<div id="payment-form"></div>
-		<input type="submit" value="Pay €1">
+	<form id="checkout" action="checkout.php" method="post">
+		<label>Cardholder name</label><input data-braintree-name="cardholder_name" value="John Doe">
+		<label>Number</label><input data-braintree-name="number" value="4111111111111111">
+		<label>CVV</label><input data-braintree-name="cvv" value="100">
+		<label>Expiration Month</label><input data-braintree-name="expiration_month" value="10">
+		<label>Expiration Year</label><input data-braintree-name="expiration_year" value="2020">
+		<label>Postal Code</label><input data-braintree-name="postal_code" value="94107">
+		<input type="submit" id="submit" value="Pay">
 	</form>
 
 	<script src="https://js.braintreegateway.com/js/braintree-2.31.0.min.js"></script>
 
 	<script>
 		braintree.setup("<?php echo $clientToken ?>", "custom", {
-			container: "payment-form"
+			id: "checkout"
 		});
 	</script>
 </body>
